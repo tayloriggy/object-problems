93fabd6c13342d37fe2de37a13997429f26704ad
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-09-14 21:35:37 -0500

    update

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+Create a sample object, then write a JavaScript function that deletes the property of an object passed to it.
+Your JavaScript function should take in two parameters, the object and the key/property you want to delete.
+
+Steps:
+1. Create an object
+2. Write a function that deletes the property that is passed to that object
+3. 
+
+*/
