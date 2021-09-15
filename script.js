aaa9a051bb473a05bc3bf7fa576ddb47ea88a218
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-09-14 21:44:06 -0500

    update

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ Create a sample object, then write a JavaScript function that deletes the proper
 Your JavaScript function should take in two parameters, the object and the key/property you want to delete.
 
 Steps:
-1. Create an object
-2. Write a function that deletes the property that is passed to that object
-3. 
+1. Create an object.
+2. Write a function that deletes the property that is passed to that object.
+3. Pass in two parameters to the function, the object and the key/property I want to delete.
+4. 
 
 */
+
+let dinosaur = {
+    species: "Triceratops",
+    type: "herbivore",
+    quirk: "three horns"
+};
