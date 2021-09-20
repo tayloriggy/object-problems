f42211be16e47e83e02083634f6d64ac60827fb3
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-09-20 17:49:45 -0500

    update

---
+++ newObject-problems/script.js
@@ -0,0 +1,43 @@
+/*
+Create an array of people objects with first name, last name, and age, then write a JavaScript program to display the 
+first and last names of all the people.
+Your function should take in an object and return the values above.
+
+Steps:
+1. Create an array of multiple people objects.
+2. Within each object, create keys such as first name, last name, and age.
+3. Define the values to each key within each person object.
+4. Use a method to display the first and last names of all people.
+5. Create a function that takes in an array of objects as its parameter.
+6. Iterate through the elements of the array of objects.
+7. Return the values of all first and last names.
+
+*/
+
+let people = [
+    {
+        firstName: 'John',
+        lastName: 'Wayne',
+        age: 45
+    },
+
+    {
+        firstName: 'William',
+        lastName: 'Bonney',
+        age: 19
+    },
+
+    {
+        firstName: 'Wyatt',
+        lastName: 'Earp',
+        age: 40
+    }
+]
+
+function fullNames (obj) {
+    for (let i = 0; i < obj.length; i++) {
+        console.log(obj[i].firstName + " " + obj[i].lastName);
+    }
+}
+
+fullNames(people);
