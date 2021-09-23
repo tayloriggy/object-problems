f36fccfeb58d679b39d2e99905de2107cd53960e
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-09-22 21:02:48 -0500

    update

--- object-problems/prob-four/script.js
+++ object-problems/prob-four/script.js
@@ -2,9 +2,18 @@
 Create an object, then write a JavaScript function that checks whether an object contains the specified key.
 
 Steps:
-1. 
-
-
-
+1. Create an object
+2. Assign several keys to the object
+3. Write a function
+4. Use a for loop to go through the keys
+5. Use an if statement to check for the specified key that is passed into the function
+6. Return true if the object contains the specified key.
 
 */
+
+const wrestler = {
+    name: "Rey Mysterio Jr.",
+    style: "acrobat",
+    signature: "hurricanrana"
+};
+
